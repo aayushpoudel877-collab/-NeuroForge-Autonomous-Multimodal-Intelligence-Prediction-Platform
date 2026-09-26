ebaf0035b238ac88fab058bce5fc2a2e36c48d1b
Aayush Poudel <aayushpoudel877@gmail.com>
2026-09-26 22:14:47 +0545

    feat: expose multimodal deep learning architecture

--- neuroforge/deep_learning/__init__.py
+++ neuroforge/deep_learning/__init__.py
@@ -1,4 +1,10 @@
-"""Trainable PyTorch components for NeuroForge."""
-from .fusion_net import MultimodalFusionNet
 from .encoders import MLPEncoder
-__all__=["MultimodalFusionNet","MLPEncoder"]
+from .fusion_net import MultimodalFusionNet
+from .modal_encoders import TextTransformerEncoder,VisionCNNEncoder,AudioCNNEncoder,TemporalTransformerEncoder
+from .cross_modal import CrossModalAttention
+from .unified_model import NeuroForgeMultimodalModel
+
+__all__=[
+    "MLPEncoder","MultimodalFusionNet","TextTransformerEncoder","VisionCNNEncoder",
+    "AudioCNNEncoder","TemporalTransformerEncoder","CrossModalAttention","NeuroForgeMultimodalModel"
+]
